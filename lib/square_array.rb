2512b1b5358dde578681a380a210cf36d6f48bd4
T Benson <t.benson@live.co.uk>
2019-11-28 12:45:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   count = 0
-  new_array = []
+  # new_array = []
 
   while count < array.length do
-    new_array.push(array[count]**2)
+    array[count] = (array[count]**2)
     count += 1
   end
 end
